c79802ab2a38203c67d7e10b55d72f5f6a589037
Kai \& Adnan <hendry+adnan@iki.fi>
2014-08-13 21:23:14 +0800

    Not working atm, message did not send

--- serial.ino
+++ serial.ino
@@ -1,11 +1,41 @@
-int incomingByte = 0;   // for incoming serial data
+#include <SoftwareSerial.h>
+#include "Akeru.h"
 
+int incomingByte = 0;
+unsigned long time;
+
+// L LED is connected to pin 13, give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
 void setup() {
-	Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-	Serial.println("Setting up");
+
+	// initialize the digital pin of L led as an output
+	pinMode(led, OUTPUT);
+
+	// initialize serial communication at 9600 bits per second:
+	Serial.begin(9600);
+	Serial.println("Starting...");
+
+	// Wait 1 second for the modem to warm
+	delay(1000);
+
+	// Init modem
+	Akeru.begin();
 }
 
 void loop() {
+
+	// Wait for availability of the modem
+	while (!Akeru.isReady()) {
+		Serial.println("Modem not ready");
+		delay(1000);
+	}
+
+	time = millis();
+	//prints time since program started
+	Serial.print("Waiting for read at ");
+	Serial.println(time);
 
 	char inbuf[16];
 	int charCtr = 0;
@@ -14,7 +44,7 @@ void loop() {
 	while (charCtr < 12) {
 		if (Serial.available() > 0) {
 
-			// read the incoming byte:
+			// read (and block) the incoming byte:
 			incomingByte = Serial.read();
 
 			if(incomingByte == '\n') {
@@ -37,4 +67,19 @@ void loop() {
 	Serial.print("Whole string: ");
 	Serial.println(inbuf);
 	Serial.flush();
+
+	// LED is on
+	digitalWrite(led, HIGH);
+
+	// Send in the mighty internet!
+	if(Akeru.send(&inbuf,sizeof(inbuf))) {
+		Serial.print("Sending message:");
+		Serial.println(inbuf);
+	} else {
+		Serial.println("Message DO NOT SEND");
+	}
+
+	// LED is off
+	digitalWrite(led, LOW);
+
 }
