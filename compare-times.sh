baaff6154f6ae719f3b17d6b9e03f9be60e71547
Kai Hendry <hendry@webconverger.com>
2014-08-15 12:59:23 +0800

    Fix for new datetime format

--- compare-times.sh
+++ compare-times.sh
@@ -2,13 +2,17 @@ curl -s 'https://docs.google.com/spreadsheets/d/1XTbKO1F6QBLg--qjekc_iMVKjFOn2Zp
 do
 	IFS=","
 	set -- $line
-	test ${#3} -eq 8 || continue
 	echo $line
-	# Fix time to be parsable
-	var="$1"; date=$(echo ${var:6:4}-${var:3:2}-${var:0:2} ${var:11:8})
-	epoch=$(date --date='TZ="Asia/Singapore" '$date +%s)
+
+	# Make sure we have a epoch sized payload
+	test ${#3} -eq 8 || continue
+
+	sendtime=$(date --date='TZ="Asia/Singapore" '$1 +%s)
+	test "$sendtime" || continue
+
 	# Fix endianness of hex
 	var=$3; var="0x${var:6:2}${var:4:2}${var:2:2}${var:0:2}"; rectime=$(printf "%d" "$var")
-	echo rectime $rectime - epoch $epoch
-	echo $(($rectime - $epoch))
+
+	echo rectime $rectime - sendtime $sendtime
+	echo $(($rectime - $sendtime))
 done
